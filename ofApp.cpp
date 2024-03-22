e2a05f2974b7ba022daaed02396ecca6faf95a1b
TommyMcPhee <135555457+TommyMcPhee@users.noreply.github.com>
2024-03-22 11:06:21 -0700

    Add files via upload

--- ofApp.cpp
+++ ofApp.cpp
@@ -1,15 +1,14 @@
 #include "ofApp.h"
 
 //--------------------------------------------------------------
-void ofApp::setup() {
+void ofApp::setup(){
 	midiSetup();
-	videoSetup();
+	
 }
 
 void ofApp::midiSetup() {
-	midiIn.openPort(inPort);
+	midiIn.openPort(port);
 	midiIn.addListener(this);
-	//midiOut.openPort(outPort);
 }
 
 void ofApp::videoSetup() {
@@ -22,53 +21,18 @@ void ofApp::videoSetup() {
 }
 
 //--------------------------------------------------------------
-void ofApp::draw() {
+void ofApp::draw(){
 	refresh();
-	updateParameters();
 	buffer.begin();
 	shader.begin();
 	setUniforms();
-	buffer.draw(0.0, 0.0);
 	shader.end();
 	buffer.end();
-	buffer.draw(0.0, 0.0);
-}
-
-void ofApp::refresh() {
-	width = (float)ofGetWidth();
-	height = (float)ofGetHeight();
-	buffer.allocate(width, height);
-	window.set(width, height);
-	ofClear(0, 0, 0, 255);
-}
-
-void ofApp::updateParameters() {
-	mainParameters.set(controls[0], controls[1], controls[2], controls[3], controls[4], controls[5], controls[6], controls[7],
-		controls[8], controls[9], controls[10], controls[11], controls[12], controls[13], controls[14], controls[15]);
-	extraParameters.set(controls[16], controls[17], controls[18]);
-	boundaryTotal = 0.0;
-	changeTotal = 0.0;	
-	total = 0.0;
-	for (int a = 0; a < controls.size(); a++) {
-		float totalIncrement = controls[a] / (float)controls.size();
-		float semiTotalIncrement = controls[a] / 8.0;
-		total += totalIncrement;
-		if (a % 4 <= 2) {
-			boundaryTotal += semiTotalIncrement;
-		}
-		else {
-			changeTotal += semiTotalIncrement;
-		}
-	}
-	smooth = (controls[16] + controls[17]) / 2.0;
-	totals.set(boundaryTotal, changeTotal, total, smooth);
+	buffer.draw(0, 0);
 }
 
 void ofApp::setUniforms() {
-	shader.setUniform2f("window", window);
-	shader.setUniformMatrix4f("mainParameters", mainParameters);
-	shader.setUniform3f("extraParameters", extraParameters);
-	shader.setUniform4f("totals", totals);
+
 }
 
 //--------------------------------------------------------------
@@ -76,11 +40,9 @@ void ofApp::newMidiMessage(ofxMidiMessage& message) {
 	if (message.status == MIDI_CONTROL_CHANGE) {
 		int control = message.control;
 		for (int a = 0; a < controlNumbers.size(); a++) {
-			if (control == controlNumbers[a]) {
-				int value = message.value;
+			if (control = controlNumbers[a]) {
 				int controlIndex = a;
-				controls[controlIndex] = (float)(value + 1) / 128.0;
-				//midiOut.sendControlChange(channel, control, value);
+				controls[controlIndex] = (float)(message.value + 1) / 128.0;
 			}
 		}
 	}
--- ofApp.h
+++ ofApp.h
@@ -3,34 +3,20 @@
 #include "ofMain.h"
 #include "ofxMidi.h"
 
-class ofApp : public ofBaseApp, ofxMidiListener {
+class ofApp : public ofBaseApp, ofxMidiListener{
 
-public:
-	void setup();
-	void midiSetup();
-	void videoSetup();
-	void draw();
-	void refresh();
-	void updateParameters();
-	void setUniforms();
-	void newMidiMessage(ofxMidiMessage& message);
-	ofxMidiIn midiIn;
-	ofxMidiOut midiOut;
-	const int inPort = 0;
-	//const int outPort = 1;
-	const int channel = 1;
-	const array<int, 19> controlNumbers = {123, 124, 125, 126, 35, 41, 44, 45, 46, 47, 51, 53, 119, 120, 121, 122, 1, 118, 7};
-	array<float, 19> controls;
-	ofShader shader;
-	ofFbo buffer;
-	ofVec2f window;
-	float width;
-	float height;
-	ofMatrix4x4 mainParameters;
-	ofVec3f extraParameters;
-	ofVec4f totals;
-	float boundaryTotal;
-	float changeTotal;
-	float total;
-	float smooth;
+	public:
+		void setup();
+		void midiSetup();
+		void videoSetup();
+		void draw();
+		void setUniforms();
+		void newMidiMessage(ofxMidiMessage& message);
+		ofxMidiIn midiIn;
+		const int port = 0;
+		array<int, 19> controlNumbers;
+		array<float, 19> controls;
+		ofShader shader;
+		ofFbo buffer;
+
 };
